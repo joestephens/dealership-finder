6faf2d76275e72331d662f5f9a48026bceac43d6
Joe Stephens <joe@webantic.co.uk>
2018-07-09 01:35:05 +0100

    Add Content component

---
+++ src/components/Content.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Content = styled.div`
+  padding: 3rem 1.5rem;
+  background-color: white;
+`
+
+/** @component */
+export default Content
---
+++ src/components/Content.md
@@ -0,0 +1,7 @@
+Content Example:
+
+```js
+<Content>
+  <Button>Button inside Content</Button>
+</Content>
+```
